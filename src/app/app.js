7b64731eeb4415119f99af0cbc7305115f85d773
Arbash Mohamad <eng@mohamadarbash.com>
2020-01-20 22:27:25 +0100

    configur webpack and babel

---
+++ src/app/app.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom';
+import Users from './containers/Users';
+
+
+const Pizza = React.lazy(() => import('./containers/Pizza.js'));
+
+
+export default class App extends Component {
+    render () {
+        return (
+            <div>
+                <div>
+                    <Link to="/">Users</Link> |
+                    <Link to="/pizza">Pizza</Link>
+                </div>
+                <div>
+                    <Route path="/" exact component={Users} />
+                    <Route path="/pizza" render={(props) =>  <Suspense fallback={<div>Loading...</div>}><Pizza {...props}/></Suspense>} />
+                </div>
+            </div>
+        );
+    }
+}
